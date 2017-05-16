491996566956c1adbb879e9238247b6d71c25b5d
ParaSLee <610033941@qq.com>
2017-05-16 19:48:49 +0800

    V2.1
    1. 新增成长知识上传
    2. 前端界面全部完成

--- 文俊霖/js/coustomJs/knowledge.js
+++ 文俊霖/js/coustomJs/knowledge.js
@@ -1,3 +1,17 @@
+var oldcover    //存储原封面
+var newcover    //存储新封面
+
+
+
+oldcover = $(".if-d-cover img").attr('src');
+
+
+
+
+
+
+
+
 /*
  *
  *   资讯管理 功能
@@ -25,7 +39,7 @@
 
 
 
-/*+++++++++++++++++++++++++++++++++++++++*/
+/*++++++++++++++   添加   +++++++++++++++++++++++++*/
 
 /*   删除弹窗动画  START*/
 
@@ -52,7 +66,134 @@
 
 
 /*    删除弹窗动画  END*/
+
+
+
+
+/*    上传封面  */
+
+	$(function(){
+		var control = $("#file-0");
+		control.fileinput({
+			language: 'zh', //设置语言
+			uploadUrl: "http://119.29.53.178:8080/kindergarden/imageUpload", //上传的地址
+			allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
+			uploadAsync: true, //默认异步上传
+			showUpload: true, //是否显示上传按钮
+			showRemove : true, //显示移除按钮
+			showPreview : true, //是否显示预览
+			showCaption: false,//是否显示标题
+			enctype: 'multipart/form-data',
+			// uploadExtraData:{wid:123},
+			browseClass: "btn btn-primary", //按钮样式
+			dropZoneEnabled: true,//是否显示拖拽区域
+			maxFileCount: 1, //表示允许同时上传的最大文件个数
+			validateInitialCount:true,
+			previewFileIcon: "<i class='glyphicon glyphicon-king'></i>",
+			msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
+			previewSettings:{width: "auto", height: "50px"},
+			textEncoding:"utf-8"
+			//{}
+
+		}).on('filepreupload', function(event, data, previewId, index) {     //上传中
+			var form = data.form, files = data.files, extra = data.extra,
+				response = data.response, reader = data.reader;
+			console.log('文件正在上传');
+		}).on("fileuploaded", function (event, data, previewId, index) {    //一个文件上传成功
+			alert('文件上传成功');
+			console.log(data.response);
+			console.log(data.response.url);
+
+			newcover = data.response.url;
+
+
+			$(".if-d-recover").css({"display":"block"});
+			$(".if-d-recover").animate({"opacity":1},700);
+
+			$(".if-d-cover img").attr('src',newcover)
+			$(".if-d-cover span").html("新封面");
+			//上传附件部分
+
+		}).on('fileerror', function(event, data, msg) {  //一个文件上传失败
+			console.log('文件上传失败！'+data.id);
+
+
+		})
+	})
+
+/*    上传封面  END*/
+
+
+/*   还原封面 按钮 */
+	$(function(){
+		$(".if-d-recover").click(function(){
+			$(".if-d-cover img").attr('src',oldcover)
+			$(".if-d-cover span").html("原封面");
+			$(".if-d-recover").animate({"opacity":0},700,function(){
+				$(".if-d-recover").css({"display":"none"});
+			});
+		})
+	})
+/*   还原封面 按钮  END*/
+
+
+/*    弹出添加框  */
+	$(function(){
+		$(".know-newpage-btn button").click(function(){
+			// 移出隐藏
+			$(".know-new-box").removeClass("myHidden");
+			$(".cover").removeClass("myHidden");
+			// 移出关闭特效
+			$(".know-new-box").removeClass("bounceOut");
+			$(".cover").removeClass("fadeOut");
+			// 增加开启特效
+			$(".know-new-box").addClass("bounceIn");
+			$(".cover").addClass("fadeIn");
+
+			// “新增”按钮不可使用
+			$(".know-newpage-btn button").attr("disable",true);
+		})
+	})
+
+/*    弹出添加框  END*/
+
+
+
+/*    取消添加  */
+	$(function(){
+		$(".know-new-no").click(function(){
+			// 移除开启特效
+			$(".know-new-box").removeClass("bounceIn");
+			$(".cover").removeClass("fadeIn");
+			// 开启关闭特效
+			$(".know-new-box").addClass("bounceOut");
+			$(".cover").addClass("fadeOut");
+			// 执行清除操作
+			$(".if-d-recover").trigger("click");
+			$(".if-d-cover-c-b .fileinput-remove-button").trigger("click");
+			$(".know-new-title input").val("");
+			$(".know-new-a input").val("");
+
+			// 动画结束后处理
+			setTimeout(function(){
+				$(".know-new-box").addClass("myHidden");
+				$(".cover").addClass("myHidden");
+				$(".know-newpage-btn button").attr("disable",false);
+			}, 1000);
+		})
+	})
+
+/*    取消添加  END*/
+
+
 /*****************************************/
+
+
+
+
+
+
+
 
 
 
