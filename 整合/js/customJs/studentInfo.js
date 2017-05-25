6771c026f2d4c9e211ed29607b9040eff775f69b
ParaSLee <610033941@qq.com>
2017-05-25 15:04:57 +0800

    整合V1.1
    整合V1.1

--- 整合/js/customJs/studentInfo.js
+++ 整合/js/customJs/studentInfo.js
@@ -416,6 +416,42 @@ function checkInput() {
         };
 
     }
+
+
+
+
+    //时间选择
+    var chooseTime = $(inputs[2]);
+    $(function () {
+        var currYear = (new Date()).getFullYear();
+        var opt={};
+        opt.date = {preset : 'date'};
+        opt.datetime = {preset : 'datetime'};
+        opt.time = {preset : 'time'};
+        opt.default = {
+            theme: 'android-ics light', //皮肤样式
+            display: 'modal', //显示方式
+            mode: 'scroller', //日期选择模式
+            dateFormat: 'yyyy-mm',
+            dateOrder: 'yymd',
+            onBeforeShow: function (inst) { inst.settings.wheels[0].length>2?inst.settings.wheels[0].pop():null; },
+            lang: 'zh',
+            showNow: true,
+            nowText: "今天",
+            startYear: currYear - 50, //开始年份
+            endYear: currYear + 10, //结束年份
+            onSelect: function (value) {
+                chooseTime.val(value);
+            }
+        };
+        chooseTime.mobiscroll($.extend(opt['date'], opt['default']));
+    });
+
+    chooseTime.click(function () {
+        $('#USER_AGE').mobiscroll('show');
+    });
+    chooseTime[0].readOnly = true;
+
 }
 
 
